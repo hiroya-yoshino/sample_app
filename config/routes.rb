a8538665019efa5e828875446dd932903a9e17b2
Hiroya Yoshino <108524504+hiroya->
2022-07-03 09:26:43 +0000

    [Finish]3-７

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
-  patch 'list/:id' => 'lists#update', as:'update_list'
   
+  get '/top' => 'homes#top'
+  resources :lists
+
 end
